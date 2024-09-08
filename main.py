9198bbe4f983520350fc907cc3189735b8748239
legendary.fish <129801050+legendaryfishwastaken@users.noreply.github.com>
2024-09-08 16:45:37 +0000

    added say command

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ client = commands.Bot(command_prefix='_', intents=intents, help_command=None)
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name="poggers"))
+    await client.change_presence(activity=discord.Streaming(url="https://movies.lfdev.site", name="movies.lfdev.site"))
     print(f'We have logged in as {client.user}')
 
 @client.command()
 async def ping(ctx):
     
-    em = discord.Embed(title=f"Ping!",description=f'''Pong!''')
-    await ctx.send(embed=em)
+    await ctx.send("Pong!")
+
+@client.command()
+async def say(ctx, msg=None):
+    if ctx.author.id == 773204048751886377 or ctx.author.id == 701621878631956572:
+        await ctx.send(msg)
+    else:
+        await ctx.send('<:chuckle:1282380972087574609>')
 
 client.run(os.environ['DISCORD_TOKEN'])
