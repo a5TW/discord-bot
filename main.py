38f702b8b3d8c6c07ae2ebe07a8ebf6418e7dbb1
legendary.fish <129801050+legendaryfishwastaken@users.noreply.github.com>
2024-09-08 16:23:04 +0000

    added a few basic things to bot

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-# This example requires the 'message_content' intent.
-
 import discord
+from discord.ext import commands
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='_', intents=intents, help_command=None)
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def ping(ctx):
+    
+    em = discord.Embed(title=f"Ping!",description=f'''Pong!''')
+    await ctx.send(embed=em)
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run('your token here')
+client.run(os.environ['DISCORD_TOKEN'])
