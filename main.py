776b8e82ada05a602faf31931593eef44f65c35b
legendary.fish <129801050+legendaryfishwastaken@users.noreply.github.com>
2024-09-08 16:23:53 +0000

    added status (i forgot to do it last time)

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ client = commands.Bot(command_prefix='_', intents=intents, help_command=None)
 
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Game(name="poggers"))
     print(f'We have logged in as {client.user}')
 
 @client.command()
